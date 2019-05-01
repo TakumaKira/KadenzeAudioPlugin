43913b99ee3fe584f9ffe96fa21a3a303499ff38
Takuma <mukudori09@gmail.com>
2019-05-01 16:37:10 +0900

    Prepared delay parameters.

--- Source/KAPDelay.cpp
+++ Source/KAPDelay.cpp
@@ -9,3 +9,32 @@
 */
 
 #include "KAPDelay.h"
+
+KAPDelay::KAPDelay()
+{
+    
+}
+
+KAPDelay::~KAPDelay()
+{
+    
+}
+
+void KAPDelay::setSampleRate(double inSampleRate)
+{
+    
+}
+
+void KAPDelay::reset()
+{
+    
+}
+
+void KAPDelay::process(float* inAudio,
+                       float inTime,
+                       float inFeedback,
+                       float inWetDry,
+                       int inNumSamplesToRender)
+{
+    
+}
