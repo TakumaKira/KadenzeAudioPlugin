c379a76b4d59760fe7cc8380fcd139c2a8e18554
Takuma <mukudori09@gmail.com>
2019-05-01 16:49:59 +0900

    Prepare some parameters for delay.

--- Source/KAPDelay.cpp
+++ Source/KAPDelay.cpp
@@ -10,7 +10,12 @@
 
 #include "KAPDelay.h"
 
+#include "JuceHeader.h"
+
 KAPDelay::KAPDelay()
+:   mSampleRate(-1),
+    mFeedbackSample(0.0),
+    mDelayIndex(0)
 {
     
 }
@@ -22,12 +27,12 @@ KAPDelay::~KAPDelay()
 
 void KAPDelay::setSampleRate(double inSampleRate)
 {
-    
+    mSampleRate = inSampleRate;
 }
 
 void KAPDelay::reset()
 {
-    
+    zeromem(mBuffer, (sizeof(double) * 2048));
 }
 
 void KAPDelay::process(float* inAudio,
@@ -37,5 +42,7 @@ void KAPDelay::process(float* inAudio,
                        float* outAudio,
                        int inNumSamplesToRender)
 {
-    
+    const float wet = inWetDry;
+    const float dry = 1.0f - wet;
+    const float feedbackMapped = jmap(inFeedback, 0.0f, 1.0f, 0.0f, 0.95f);
 }
