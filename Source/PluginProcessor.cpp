a103147388c309e389d7d27b13e93844fb7b025b
Takuma <mukudori09@gmail.com>
2019-05-02 14:50:10 +0900

    Fixed null error.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -24,6 +24,7 @@ KadenzeAudioPluginAudioProcessor::KadenzeAudioPluginAudioProcessor()
                        )
 #endif
 {
+    initializeDSP();
 }
 
 KadenzeAudioPluginAudioProcessor::~KadenzeAudioPluginAudioProcessor()
