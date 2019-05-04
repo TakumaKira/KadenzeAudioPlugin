0a42a10d430d14dbec049a42a9951f4cc0157a07
Takuma <mukudori09@gmail.com>
2019-05-04 13:43:54 +0900

    Prepare slider and combobox.

--- Source/KAPParameterSlider.h
+++ Source/KAPParameterSlider.h
@@ -9,3 +9,21 @@
 */
 
 #pragma once
+
+#include "JuceHeader.h"
+
+class KAPParameterSlider
+:   public Slider
+{
+public:
+    
+    KAPParameterSlider(AudioProcessorValueTreeState& stateToControl,
+                       const String& parameterID);
+    
+    ~KAPParameterSlider();
+    
+private:
+    
+    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> mAttachment;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(KAPParameterSlider);
+};
