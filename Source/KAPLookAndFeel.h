9aa7b1d19b84de54707ec19945bb2ff02957f3b7
Takuma <mukudori09@gmail.com>
2019-05-05 22:47:19 +0900

    Prepare to set colors.

--- Source/KAPLookAndFeel.h
+++ Source/KAPLookAndFeel.h
@@ -17,8 +17,44 @@ class KAPLookAndFeel
 {
 public:
     
-    KAPLookAndFeel();
+    KAPLookAndFeel()
+    {
+        // comboBox colours
+        setColour(ComboBox::backgroundColourId, KAPColour_3);
+        setColour(ComboBox::outlineColourId, KAPColour_2);
+        setColour(ComboBox::arrowColourId, KAPColour_1);
+        setColour(ComboBox::textColourId, KAPColour_1);
+        
+        // button text colours
+        setColour(TextButton::buttonColourId, KAPColour_1);
+        setColour(TextButton::textColourOnId, KAPColour_1);
+        setColour(TextButton::textColourOffId, KAPColour_1);
+    }
+    
     ~KAPLookAndFeel();
+    
+    Font getTextButtonFont (TextButton&, int buttonHeight) override
+    {
+        return font_1;
+    }
+    
+    void drawButtonBackground (Graphics& g, Button& button, const Colour& backgroundColour,
+                               bool isMouseOverButton, bool isButtonDown) override
+    {
+        Colour fillColour;
+        
+        if (isButtonDown) {
+            fillColour = KAPColour_5;
+        } else {
+            fillColour = KAPColour_3;
+        }
+        
+        const float cornerSize = 6.0f;
+        const Rectangle<float> bounds = button.getLocalBounds().toFloat().reduced(0.5f, 0.5f);
+        
+        g.setColour(fillColour);
+        g.fillRoundedRectangle(bounds.reduced(1), cornerSize);
+    };
     
 private:
 };
