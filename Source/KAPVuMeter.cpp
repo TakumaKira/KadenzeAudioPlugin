9d8586ce142711538f9df42285b9b87920e5f3f1
Takuma <mukudori09@gmail.com>
2019-05-06 15:20:56 +0900

    Prepared the box for VU meter.

--- Source/KAPVuMeter.cpp
+++ Source/KAPVuMeter.cpp
@@ -9,11 +9,12 @@
 */
 
 #include "KAPVuMeter.h"
+#include "KAPInterfaceDefines.h"
 
 KAPVuMeter::KAPVuMeter(KadenzeAudioPluginAudioProcessor* inProcessor)
-:   mCh0Level(0),
+:   mParameterID(-1),
+    mCh0Level(0),
     mCh1Level(0),
-    mParameterID(-1),
     mProcessor(inProcessor)
 {
     
@@ -26,7 +27,28 @@ KAPVuMeter::~KAPVuMeter()
 
 void KAPVuMeter::paint(Graphics& g)
 {
+    const int meter_width = getWidth() / 3;
     
+    g.setColour(KAPColour_6);
+    // left
+    g.fillRect(0, 0, meter_width, getHeight());
+    // right
+    g.fillRect(meter_width * 2, 0, meter_width, getHeight());
+    
+    int ch0fill = getHeight() - (getHeight() - mCh0Level);
+    int ch1fill = getHeight() - (getHeight() - mCh1Level);
+    
+    if (ch0fill < 0) {
+        ch0fill = 0;
+    }
+    
+    if (ch1fill < 0) {
+        ch1fill = 0;
+    }
+    
+    g.setColour(KAPColour_7);
+    g.fillRect(0, ch0fill, meter_width, getHeight());
+    g.fillRect(meter_width*2, ch1fill, meter_width, getHeight());
 }
 
 void KAPVuMeter::timerCallback()
@@ -37,4 +59,6 @@ void KAPVuMeter::timerCallback()
 void KAPVuMeter::setParameterID(int inParameterID)
 {
     mParameterID = inParameterID;
+    
+    startTimerHz(15);
 }
