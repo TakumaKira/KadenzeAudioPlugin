a49abb74b4c84cbf17ec13e81987f50b0d050c8b
Takuma <mukudori09@gmail.com>
2019-05-06 15:40:41 +0900

    Prepare meter logic.

--- Source/KAPAudioHelpers.h
+++ Source/KAPAudioHelpers.h
@@ -12,6 +12,7 @@
 
 #define kParameterSmoothingCoeff_Generic 0.04
 #define kParameterSmoothingCoeff_Fine 0.002
+#define kMeterSmoothingCoeff 0.2
 
 const static int maxBufferDelaySize = 192000;
 
@@ -22,4 +23,17 @@ const static double k2PI = 6.28318530718;
 inline float kap_linear_interp(float v0, float v1, float t)
 {
     return (1 - t) * v0 + t * v1;
+}
+
+inline float kap_denormalize(float inValue)
+{
+    float absValue = fabs(inValue);
+    
+    // 0.000000000000001
+    if (absValue < 1e-15)
+    {
+        return 0.0f;
+    } else {
+        return inValue;
+    }
 }
