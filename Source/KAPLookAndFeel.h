2be379c25fe5451621f0f0f9b4a022715caf08cc
Takuma <mukudori09@gmail.com>
2019-05-06 12:54:26 +0900

    Added knob image.

--- Source/KAPLookAndFeel.h
+++ Source/KAPLookAndFeel.h
@@ -115,7 +115,24 @@ public:
                            float sliderPosProportional, float rotaryStartAngle,
                            float rotaryEndAngle, Slider& slider) override
     {
+        const int numFrames = mSliderImage.getHeight() / mSliderImage.getWidth();
+        const int frameIndex = (int)std::ceil(sliderPosProportional * (numFrames - 1));
         
+        const float radius = jmin(width*0.5, height*0.5);
+        const float centreX = x + width * 0.5f;
+        const float centreY = y + height * 0.5f;
+        const float rx = centreX - radius;
+        const float ry = centreY - radius;
+        
+        g.drawImage(mSliderImage,
+                    rx,
+                    ry,
+                    2*radius,
+                    2*radius,
+                    0,
+                    frameIndex * mSliderImage.getWidth(),
+                    mSliderImage.getWidth(),
+                    mSliderImage.getWidth());
     }
     
 private:
